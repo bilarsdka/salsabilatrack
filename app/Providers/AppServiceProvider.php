89eb4ee08aca3f6b9fd046a243332560b5880bd4
bilarsdka <rosdikanabila6@gmail.com>
2026-06-13 13:58:33 +0700

    providers udpdate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,8 +11,10 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    public function boot()
+    public function boot(): void
     {
-        //
+    if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
     }
+}   
 }
